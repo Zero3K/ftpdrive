55a45ad29396975e51b40fda29e87ad29df8bfca
killer-r <support@killprog.com>
2017-04-23 20:09:38 +0300

    recommit start

--- FtpFS/PidsWatcher.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-namespace PidsHandler
- {
- void HandleOpened(DWORD pid,DWORD fs_handle);
- void HandleClosed(DWORD pid,DWORD fs_handle);
- }
--- go.cmd
+++
@@ -1,8 +0,0 @@
-set STL=D:\Dev\STLport-5.2.1
-set BOOST=D:\Dev\boost_1_36_0
-set MSSDK=D:\Dev\MSSDK_2005
-set DDKROOT=D:\Dev\WINDDK\3790.1830
-set OPENSSL=D:\Dev\OpenSSL
-
-start msdev FtpDrive.dsw
-
